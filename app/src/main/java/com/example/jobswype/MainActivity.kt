ac49f86ef9388225b312234a8175ba98e795dca9
Dr4gonJnuit <jonas.bette@student.uclouvain.be>
2024-03-03 21:03:36 +0100

    Ajout de tous les fichiers nécessaires
    Ces fichiers sont ceux qui permettent normalement de lancer l'application sur mon ordinateur

--- app/src/main/java/com/example/jobswype/MainActivity.kt
+++ app/src/main/java/com/example/jobswype/MainActivity.kt
@@ -7,24 +7,15 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.jobswype.ui.theme.JobSwypeTheme
 import kotlin.math.abs
 
@@ -38,22 +29,18 @@ class MainActivity : ComponentActivity(), GestureDetector.OnGestureListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-/*
+
         setContent {
             JobSwypeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier,
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingImage(
-                        stringResource(R.string.happy_birthday_text),
-                        stringResource(R.string.signature_text)
-                    )
+                    SwipeZone(modifier = Modifier)
                 }
             }
         }
- */
 
         gestureDetector = GestureDetector(this, this)
     }
@@ -125,58 +112,20 @@ class MainActivity : ComponentActivity(), GestureDetector.OnGestureListener {
 }
 
 @Composable
-fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
-    // Create a column so that texts don't overlap
-    Column(
-        verticalArrangement = Arrangement.Center,
-        modifier = modifier
-    ) {
-        Text(
-            text = message,
-            fontSize = 100.sp,
-            lineHeight = 116.sp,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.padding(top = 16.dp)
-        )
-        Text(
-            text = from,
-            fontSize = 36.sp,
-            modifier = Modifier
-                .padding(top = 16.dp)
-                .padding(end = 16.dp)
-                .align(alignment = Alignment.End)
-
-        )
-    }
-}
-
-@Composable
-fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
-    // Create a box to overlap image and texts
+fun SwipeZone(modifier: Modifier) {
     Box(modifier) {
         Image(
-            painter = painterResource(id = R.drawable.androidparty),
+            painter = painterResource(id = R.drawable.kotlin),
             contentDescription = null,
-            contentScale = ContentScale.Crop,
-            alpha = 0.5F
-        )
-        GreetingText(
-            message = message,
-            from = from,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(8.dp)
+            contentScale = ContentScale.Crop
         )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-private fun BirthdayCardPreview() {
+fun SwipeZonePreview() {
     JobSwypeTheme {
-        GreetingImage(
-            stringResource(R.string.happy_birthday_text),
-            stringResource(R.string.signature_text)
-        )
+        SwipeZone(Modifier)
     }
 }
