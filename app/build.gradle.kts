e3e6e2e2b7f77ad46c590a3bbf0137229de21b93
Gloria <gloria.Akli-kodjo@student.uclouvain.be>
2024-03-09 13:26:49 +0100

    Login + Sign in + Swipe OK (MAIS quand in retourne en arrière revient sur login)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,15 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.example.jobswype"
+    namespace = "com.example.loginsignup"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.jobswype"
+        applicationId = "com.example.loginsignup"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
@@ -23,7 +24,10 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
@@ -35,6 +39,7 @@ android {
     }
     buildFeatures {
         compose = true
+        viewBinding = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
@@ -48,8 +53,6 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-splashscreen:1.0.1")
-    implementation ("androidx.viewpager2:viewpager2:1.0.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -58,6 +61,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.firebase.auth)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
